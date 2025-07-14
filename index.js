cb3b5d1188a96b06f3e66d2f736986240176d2f4
ContentBakery1 <keke.zuidinga@icloud.com>
2025-07-14 14:24:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-let latestUser = { username: "Loading..." };
+let latestUser = { username: "Loading...", id: 0 };
+
+// Cache de laatst gevonden userId om niet steeds vanaf 4 miljard te zoeken
+let lastCheckedId = 4000000000;
 
 async function fetchLatestUser() {
-  for (let id = 4000000000; id > 1; id--) {
+  for (let id = lastCheckedId; id > 1; id--) {
     try {
       const res = await axios.get(`https://users.roproxy.com/v1/users/${id}`);
       if (res.data && res.data.name && !res.data.isBanned) {
-        latestUser = { username: res.data.name };
+        latestUser = { username: res.data.name, id: id };
+        lastCheckedId = id - 1; // ga volgende keer iets lager zoeken
         break;
       }
-    } catch (_) {}
+    } catch (_) {
+      // fout negeren en doorgaan
+    }
   }
 }
 
-setInterval(fetchLatestUser, 1000); // update elke 1 seconde
+setInterval(fetchLatestUser, 1000); // update elke seconde
 fetchLatestUser();
 
 app.get('/newest', (req, res) => {
