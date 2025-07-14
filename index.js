610c4ec427e46db8c5ad800eb5657e928b417a78
ContentBakery1 <keke.zuidinga@icloud.com>
2025-07-14 13:51:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+let latestUser = { username: "Loading..." };
+
+async function fetchLatestUser() {
+  for (let id = 4000000000; id > 1; id--) {
+    try {
+      const res = await axios.get(`https://users.roproxy.com/v1/users/${id}`);
+      if (res.data && res.data.name && !res.data.isBanned) {
+        latestUser = { username: res.data.name };
+        break;
+      }
+    } catch (_) {}
+  }
+}
+setInterval(fetchLatestUser, 5 * 60 * 1000); // update elke 5 min
+fetchLatestUser();
+
+app.get('/newest', (req, res) => {
+  res.json(latestUser);
+});
+
+app.get('/', (req, res) => {
+  res.send("✅ Roblox Newest Account API running");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Running on ${port}`));
