4be4e0a668056fa1308a3097bbe085db9cada358
ContentBakery1 <keke.zuidinga@icloud.com>
2025-07-16 14:40:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fetch from 'node-fetch';
 
 const app = express();
-let maxId = 8935320620;
+let maxId = 8935427500;
 let latest = { userId: 0, username: "" };
 
 async function fetchUser(id) {
